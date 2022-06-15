b4d73ec933421cb6941bead061b3b828bb283689
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-06-15 11:19:19 +0200

    Terminata classe Product con calcolo iva e frase finale

--- src/jana60/shop/Product.java
+++ src/jana60/shop/Product.java
@@ -1,19 +1,38 @@
 package jana60.shop;
 
+import java.text.DecimalFormat;
+
 public class Product {
+
 	public Product() {
 	}
+	// Creazione variabili base
 
 	String marca, nome;
 	float prezzo;
-	int Iva;
+	int iva;
+
+	// Formattatore decimale
+	DecimalFormat df = new DecimalFormat("#.00€");
+
+	// Definizione metodo immissione dati
 
 	public Product(String marca, String nome, float prezzo, int iva) {
 		super();
 		this.marca = marca;
 		this.nome = nome;
 		this.prezzo = prezzo;
-		Iva = iva;
+		this.iva = iva;
 	}
 
+	// Calcolo del prezzo Iva
+	public float calcolaIva() {
+		return prezzo + ((prezzo / 100) * iva);
+	}
+
+	// Per stampare la frase finale con prodotto
+	public String fraseFinale() {
+
+		return "Il prezzo finale del prodotto " + nome + " della marca " + marca + " è di: " + df.format(calcolaIva());
+	}
 }
